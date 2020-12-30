6599b6f19523d3df903fec624fa28e9be197ed63
vitorsilvalima <vitor.lima@klarna.com>
2020-12-30 10:18:53 -0300

    navigation: add new tabs

--- src/navigation/app.tsx
+++ src/navigation/app.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { TabBar } from "./components/TabBar";
 import MovieHome from "@features/movie/screens/MovieHome";
+import { Ionicons } from "@expo/vector-icons";
+import Colors from "@shared/constants/Colors";
+import { TabBarIcon } from "./components/TabBarIcon";
 
 export const APP_ROUTES = {
   itself: "app",
@@ -12,12 +15,53 @@ export const APP_ROUTES = {
 
 export type AppParamList = {
   [APP_ROUTES.movieList]: undefined;
+  [APP_ROUTES.profile]: undefined;
+  [APP_ROUTES.search]: undefined;
 };
 
 const AppTabNavigator = createBottomTabNavigator<AppParamList>();
 
 export const AppStack = () => (
   <AppTabNavigator.Navigator tabBar={TabBar}>
-    <AppTabNavigator.Screen name={APP_ROUTES.movieList} component={MovieHome} />
+    <AppTabNavigator.Screen
+      name={APP_ROUTES.movieList}
+      component={MovieHome}
+      options={{
+        tabBarIcon: TabBarIcon(
+          <Ionicons
+            name="videocam"
+            color={Colors.dark.primaryColor}
+            size={24}
+          />,
+          <Ionicons name="videocam-outline" color={"#686868"} size={24} />
+        ),
+      }}
+    />
+
+    <AppTabNavigator.Screen
+      name={APP_ROUTES.search}
+      component={MovieHome}
+      options={{
+        tabBarIcon: TabBarIcon(
+          <Ionicons name="search" color={Colors.dark.primaryColor} size={24} />,
+          <Ionicons name="search-outline" color={"#686868"} size={24} />
+        ),
+      }}
+    />
+
+    <AppTabNavigator.Screen
+      name={APP_ROUTES.profile}
+      component={MovieHome}
+      options={{
+        tabBarIcon: TabBarIcon(
+          <Ionicons
+            name="person-circle"
+            color={Colors.dark.primaryColor}
+            size={24}
+          />,
+          <Ionicons name="person-circle-outline" color={"#686868"} size={24} />
+        ),
+      }}
+    />
   </AppTabNavigator.Navigator>
 );
